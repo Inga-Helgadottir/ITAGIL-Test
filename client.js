65912aab4d0fb994a9cd3f55d012adf7d64b4ac1
unknown <sigurroshelgadottir@gmail.com>
2024-05-02 15:22:26 +0200

    added a func called checkForEmptyListAndAddSpaceForNewTask, because it was impossible to add tasks to an empty list

--- client.js
+++ client.js
@@ -3,14 +3,34 @@ $(document).ready(function () {
   $("h2").css("padding", "10px");
   $("h2").css("text-align", "center");
 
+  function checkForEmptyListAndAddSpaceForNewTask() {
+    $(".item-container").each(function (index) {
+      if (this.children.length === 0) {
+        this.style.height = "20px";
+      } else if (
+        this.className === "todo-item-container item-container ui-droppable" &&
+        this.children.length === 1
+      ) {
+        this.style.height = "20px";
+      } else {
+        this.style.height = "";
+      }
+    });
+  }
+
   $(".single-item").draggable();
 
   $(".item-container").droppable({
-    drop: function (event, ui) {
+    drop: function (event, element) {
       var closestContainer = $(event.target).closest(".item-container");
-      var movingTask = ui.draggable[0];
+      var movingTask = element.draggable[0];
+
       movingTask.style.position = "static";
       closestContainer.append(movingTask);
+      checkForEmptyListAndAddSpaceForNewTask();
+    },
+    stop: function (event, ui) {
+      movingTask.style.position = "reletive";
     },
   });
 
@@ -44,7 +64,12 @@ $(document).ready(function () {
   }
 });
 
+//unit test google jest
 //https://web.dev/articles/drag-and-drop
 //https://trello.com/b/lhQFdopW/itagil-test
 //https://stackoverflow.com/questions/17079615/jquery-draggable-droppable-get-source-container-that-draggable-came-from
 //https://jsfiddle.net/wL4pkp9m/
+//https://api.jquery.com/each/
+//https://www.elated.com/drag-and-drop-with-jquery-your-essential-guide/
+//https://gsap.com/community/forums/topic/17775-best-way-to-get-a-draggable-element-out-of-a-scrollable-area-draggable-bug/
+//https://stackoverflow.com/questions/11944270/only-allowing-one-droppable-area-jquery-drag-and-drop
