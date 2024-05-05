f5a97c6502809dbef80846cf7871714e934fa724
S. Inga Helgadottir <cph-sh557@cphbusiness.dk>
2024-05-05 16:47:52 +0200

    tried to add to the xml again, didnt work, i will try again tomorrow

--- client.js
+++ client.js
@@ -3,47 +3,110 @@ $(document).ready(function () {
   $("h2").css("padding", "10px");
   $("h2").css("text-align", "center");
 
-  $(".add-new-task-form").submit(function (e) {
-    e.preventDefault();
-    var inputValueTask = $("#add-task-input").val();
-    var inputValueCompany = $("#add-company-input").val();
-    var inputValueType = $("#add-type-input").val();
-    if (
-      inputValueTask === "" ||
-      inputValueCompany === "" ||
-      inputValueType === ""
-    ) {
-      alert("You need to add the relevant information: task, company and type");
-    } else {
-      addNewTask(inputValueTask, inputValueCompany, inputValueType);
-      $("#add-task-input").val("");
-      $("#add-company-input").val("");
-      $("#add-type-input").val("");
-    }
-  });
+  // var xhttp = new XMLHttpRequest();
+  // xhttp.onreadystatechange = function () {
+  //   if (this.readyState == 4 && this.status == 200) {
+  //     myFunction(this);
+  //   }
+  // };
+  // xhttp.open("GET", "input.xml", true);
+  // xhttp.send();
+  myFunction();
+  // function myFunction(xml) {
+  function myFunction() {
+    //   console.log(xml);
+    //   var xmlDoc;
+    //   // var x, y, i, newElement, txt, xmlDoc;
+    //   xmlDoc = xml.responseXML;
+    // newElement = xmlDoc.createElement("edition");
+    // x = xmlDoc.getElementsByTagName("book")[0]
+    let issue = {
+      title: "test",
+      type: "issuesssssss",
+      state: "todo",
+      company: "comptest",
+    };
 
-  function addNewTask(task, company, type) {
-    var singleItem = $(
-      '<div class="single-item" draggable="true"> <div class="header">' +
-        task +
-        "</div><div>" +
-        company +
-        "</div></div>"
+    // ("<issue><title>testing</title><type>issue</type><state>todo</state><company>Company22222</company></issue>");
+    let newElement =
+      "<issue><title>testing</title><type>issue</type><state>todo</state><company>Company22222</company></issue>";
+    // // let newElement =
+    // //   "<issue><title>testing</title><type>issue</type><state>todo</state><company>Company22222</company></issue>";
+    // var parser = new DOMParser();
+    // // console.log(newElement);
+    // let elementParsed = parser.parseFromString(issue, "text/xml");
+    // // // let elementParsed = jQuery.parseXML(newElement);
+    // // console.log(elementParsed);
+
+    $.post(
+      "/input.xml",
+      "<issue><title>testing</title><type>issue</type><state>todo</state><company>Company22222</company></issue>"
     );
 
-    $(".todo-item-container").append(singleItem);
-    // add to xml here--------------------------------------------------------
+    // $.ajax({
+    //   headers: {
+    //     "Access-Control-Allow-Origin": "*",
+    //     "Access-Control-Allow-Methods": "GET, POST",
+    //     "Access-Control-Allow-Credentials": "true",
+    //   },
+    //   url: "/input.xml",
+    //   data: issue,
+    //   contentType: "text/xml",
+    //   dataType: "text/xml",
+    //   type: "POST",
+    //   success: function (result) {
+    //     console.log(result);
+    //   },
+    //   error: function (xhr, ajaxOptions, thrownError) {
+    //     console.log(xhr.status);
+    //     console.log(thrownError);
+    //   },
+    // });
+
+    // x.appendChild(newElement);
   }
 
-  function checkForEmptyListAndAddSpaceForNewTask() {
-    $(".item-container").each(function (index) {
-      if (this.children.length === 0) {
-        this.style.height = "20px";
-      } else {
-        this.style.height = "";
-      }
-    });
-  }
+  // $(".add-new-task-form").submit(function (e) {
+  //   e.preventDefault();
+  //   var inputValueTask = $("#add-task-input").val();
+  //   var inputValueCompany = $("#add-company-input").val();
+  //   var inputValueType = $("#add-type-input").val();
+  //   if (
+  //     inputValueTask === "" ||
+  //     inputValueCompany === "" ||
+  //     inputValueType === ""
+  //   ) {
+  //     alert("You need to add the relevant information: task, company and type");
+  //   } else {
+  //     addNewTask(inputValueTask, inputValueCompany, inputValueType);
+  //     $("#add-task-input").val("");
+  //     $("#add-company-input").val("");
+  //     $("#add-type-input").val("");
+  //   }
+  // });
+
+  // function addNewTask(task, company, type) {
+  //   var singleItem = $(
+  //     '<div class="single-item" draggable="true"> <div class="header">' +
+  //       task +
+  //       "</div><div>" +
+  //       company +
+  //       "</div></div>"
+  //   );
+
+  //   $(".todo-item-container").append(singleItem);
+  //   // add to xml here--------------------------------------------------------
+  // }
+
+  // function checkForEmptyListAndAddSpaceForNewTask() {
+  //   $(".item-container").each(function (index) {
+  //     if (this.children.length === 0) {
+  //       this.style.height = "20px";
+  //     } else {
+  //       this.style.height = "";
+  //     }
+  //   });
+  // }
 });
 
 //unit test google jest
@@ -62,3 +125,7 @@ $(document).ready(function () {
 //https://www.linkedin.com/learning/xml-essential-training-2/xml-and-css-styles?autoSkip=true&resume=false
 //https://www.linkedin.com/learning/learning-jquery-ui/next-steps?autoSkip=true&resume=false
 //https://www.linkedin.com/learning/learn-api-documentation-with-json-and-xml/welcome
+
+//https://www.youtube.com/watch?v=dqOxpBG28io
+//https://www.youtube.com/watch?v=fEYx8dQr_cQ
+//https://www.youtube.com/watch?v=5nL7X1UMWsc
